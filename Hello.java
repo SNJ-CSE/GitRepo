50fddc2cf3072926cef1bf5d77444892caecb85c
Sunilkumar Jaiswal <sjaiswal@mgmu.ac.in>
2023-03-02 16:00:08 +0530

    Hello.java modified

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ class Hello
 {
 public static void main(String args[])
 {
+	System.out.println("Second Message added to Hello.java");
 	System.out.println("Testing Java Application Using Jnekins");
 }
 
